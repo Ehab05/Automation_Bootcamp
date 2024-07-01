8d524169131fc00771d38ca8b0fe94f5a3368983
ehab05 <ehabkalil5@gmail.com>
2024-07-01 16:34:43 +0300

    google search exercise

--- selenium/test_1.py
+++ selenium/test_1.py
@@ -1,1 +1,23 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
+
+def test_google_search():
+    try:
+        driver = webdriver.Chrome(service=Service(r"E:\5-tech\Autmation\python_selenium\chromedriver\chromedriver.exe"))
+        driver.get("http://www.google.com/")
+        driver.implicitly_wait(10)
+        search_field = driver.find_element(By.NAME, "q")
+        search_field.send_keys("Python programming", Keys.ENTER)
+        search_result = driver.find_element(By.CSS_SELECTOR, "h3.LC20lb.MBeuO.DKV0Md")
+        assert "Python For Beginners" in search_result.text
+        print(f"Yay the test has passed we found : {search_result.text}")
+
+    except Exception as e:
+        print(f"we didn't find it sorry:{e}")
+
+    finally:
+        driver.quit()
